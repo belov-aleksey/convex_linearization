ea1c3edb9f2aa4781aa8bf5466f12822e7aaa9a5
Aleksey Belov <aleshabelov2001@mail.ru>
2024-04-07 23:30:08 +0400

    Первая версия для решения тестовой задачи для функции двух переменных

--- conlin.jl
+++ conlin.jl
@@ -14,7 +14,7 @@ dgdx2(x) = -72/(16*x[1]+9*x[2])^2+72/(9*x[1]+16*x[2])^2
 dgdx = [dgdx1, dgdx2]
 
 x1_range=[0.2,2.5]
-x2_range=[0.2, 2.5]
+x2_range=[0.2,2.5]
 x0=[2,1]
 alpha=0.2
 beta=2.5
@@ -77,8 +77,6 @@ function xi_opt(lam, Ai, Bi, alpha, beta, DLdxi)
     end
 end
 
-phi(lam) = Vc([xi_opt(lam,A1,B1,alpha,beta,dldx1), xi_opt(lam,A2,B2,alpha,beta,dldx2)])+lam*gc([xi_opt(lam,A1,B1,alpha,beta,dldx1),xi_opt(lam,A2,B2,alpha,beta,dldx2)])
-
 function conlin_approximate(f, dfdx::Vector, x0)
     p = []
     q = []
@@ -100,40 +98,82 @@ function conlin_approximate(f, dfdx::Vector, x0)
     return fc
 end
 
+function conlin_iter(
+    V,
+    dVdx,
+    g,
+    dgdx,
+    x0,
+    alpha,
+    beta
+)
+    # CONLIN-аппроксимация целевой функции и функции ограничений
+    gc = conlin_approximate(g, dgdx, x0)
+    Vc = conlin_approximate(V, dVdx, x0)
 
-# CONLIN-аппрокисмации целевой функции и функции ограничений
-gc = conlin_approximate(g, dgdx, x0)
-Vc = conlin_approximate(V, dVdx, x0)
+    # pi, pji, qi, qji
+    p_v1=pi(dVdx1,x0)
+    p_v2=pi(dVdx2,x0)
+    q_v1=qi(dVdx1,x0,x0[1])
+    q_v2=qi(dVdx2,x0,x0[2])
 
-# pi, pji, qi, qji
-p_v1=pi(dVdx1,x0)
-p_v2=pi(dVdx2,x0)
-q_v1=qi(dVdx1,x0,x0[1])
-q_v2=qi(dVdx2,x0,x0[2])
+    p_g1=pi(dgdx1,x0)
+    p_g2=pi(dgdx2,x0)
+    q_g1=qi(dgdx1,x0,x0[1])
+    q_g2=qi(dgdx2,x0,x0[2])
 
-p_g1=pi(dgdx1,x0)
-p_g2=pi(dgdx2,x0)
-q_g1=qi(dgdx1,x0,x0[1])
-q_g2=qi(dgdx2,x0,x0[2])
+    # Ai, Bi
+    A1=Ai(p_v1, p_g1)
+    A2=Ai(p_v2, p_g2)
+    B1=Bi(q_v1, q_g1)
+    B2=Bi(q_v2, q_g2)
 
-# Ai, Bi
-A1=Ai(p_v1, p_g1)
-A2=Ai(p_v2, p_g2)
-B1=Bi(q_v1, q_g1)
-B2=Bi(q_v2, q_g2)
+    # Градиент Лагранжиана
+    dldx1 = dLdxi(A1,B1)
+    dldx2 = dLdxi(A2,B2)
 
-# Градиент Лагранжиана
-dldx1 = dLdxi(A1,B1)
-dldx2 = dLdxi(A2,B2)
+    # Функция phi(lambda)
+    phi(lam) = Vc([xi_opt(lam,A1,B1,alpha,beta,dldx1), xi_opt(lam,A2,B2,alpha,beta,dldx2)])+lam*gc([xi_opt(lam,A1,B1,alpha,beta,dldx1),xi_opt(lam,A2,B2,alpha,beta,dldx2)])
+    lambda_vector=range(0, stop=30,length=100)
+    phi_vector=[phi(lam) for lam in lambda_vector]
 
-# Функция phi(lambda)
-lambda_vector=range(0, stop=30,length=100)
-phi_vector=[phi(lam) for lam in lambda_vector]
-p = plot(lambda_vector,phi_vector)
-savefig(p,"phi.png")
+    # Функция -phi (метод золотого сечения ищет только минимум, нужен максимум)
+    phi_(lam) = -(Vc([xi_opt(lam,A1,B1,alpha,beta,dldx1), xi_opt(lam,A2,B2,alpha,beta,dldx2)])+lam*gc([xi_opt(lam,A1,B1,alpha,beta,dldx1),xi_opt(lam,A2,B2,alpha,beta,dldx2)]))
+    lambda_opt_iter = optimize(phi_, 0.0, 100.0, GoldenSection())
 
-# Функция -phi (метод золотого сечения ищет только минимум, нужен максимум)
-phi_(lam) = -(Vc([xi_opt(lam,A1,B1,alpha,beta,dldx1), xi_opt(lam,A2,B2,alpha,beta,dldx2)])+lam*gc([xi_opt(lam,A1,B1,alpha,beta,dldx1),xi_opt(lam,A2,B2,alpha,beta,dldx2)]))
-lambda_opt_iter = optimize(phi_, 0.0, 100.0, GoldenSection())
-x1_iter = xi_opt(lambda_opt_iter.minimizer,A1,B1,alpha,beta,dldx1)
-x2_iter = xi_opt(lambda_opt_iter.minimizer,A2,B2,alpha,beta,dldx2)
+    # Проектные переменные
+    x1_iter = xi_opt(lambda_opt_iter.minimizer,A1,B1,alpha,beta,dldx1)
+    x2_iter = xi_opt(lambda_opt_iter.minimizer,A2,B2,alpha,beta,dldx2)
+    return [x1_iter, x2_iter]
+end
+
+norm(vector) = sqrt(sum(vector.^2))
+function conlin(
+    V,
+    dVdx,
+    g,
+    dgdx,
+    x0,
+    alpha,
+    beta    
+)
+    x = conlin_iter(V, dVdx, g, dgdx, x0,alpha, beta)
+    i = 1
+    while norm(abs.(x-x0))>=0.01 && i < 100
+        println("x=$x")
+        x0 = x
+        x = conlin_iter(V, dVdx, g, dgdx, x,alpha, beta)
+    end
+end
+
+
+
+conlin(
+    V,
+    dVdx,
+    g,
+    dgdx,
+    x0,
+    alpha,
+    beta    
+)
